503b4baaca7a1466967b580417a8af8a5032a787
Your Name <youremail@example.com>
2025-08-12 10:59:40 +0600

    Update

--- lib/features/navigationbar/controller/bottom_navigationbar_controller.dart
+++ lib/features/navigationbar/controller/bottom_navigationbar_controller.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:inprep_ai/features/home_screen/screen/home_screen.dart';
-import 'package:inprep_ai/features/interview/interview_lists/view/interview_list_view.dart';
 import 'package:inprep_ai/features/profile_screen/screen/profile_screen.dart';
 import 'package:inprep_ai/features/progress_screen/screen/progress_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart'
@@ -19,7 +18,8 @@ class BottomNavbarController extends GetxController {
       case 0:
         return HomeScreen();
       case 1:
-        return InterviewListView();
+        return Container();
+        
       case 2:
         return ProgressScreen();
       default:
